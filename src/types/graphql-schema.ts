14075a3157215e81a4ffb66582cda6aaefc25602
Aleksander98 <aleksander.woznica98@gmail.com>
2024-02-20 09:01:39 +0100

    Add codegen

---
+++ src/types/graphql-schema.ts
@@ -0,0 +1,52 @@
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  AWSDate: { input: string; output: string; }
+  AWSDateTime: { input: string; output: string; }
+  AWSEmail: { input: string; output: string; }
+  AWSIPAddress: { input: string; output: string; }
+  AWSJSON: { input: string; output: string; }
+  AWSPhone: { input: string; output: string; }
+  AWSTime: { input: string; output: string; }
+  AWSTimestamp: { input: number; output: number; }
+  AWSURL: { input: string; output: string; }
+};
+
+export type AddEventInput = {
+  content: Scalars['String']['input'];
+  userId: Scalars['String']['input'];
+};
+
+export type Event = {
+  __typename?: 'Event';
+  content: Scalars['String']['output'];
+  createdAt: Scalars['AWSDateTime']['output'];
+  id: Scalars['String']['output'];
+  userId: Scalars['String']['output'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  addEvent?: Maybe<Event>;
+};
+
+
+export type MutationAddEventArgs = {
+  event: AddEventInput;
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  onEvent?: Maybe<Event>;
+};
